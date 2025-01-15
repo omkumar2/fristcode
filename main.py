8b26b747170d3ba9b934f8fd89ba4ce3e5d77f24
omkumar2 <omkumariitbomboy@gmail.com>
2025-01-15 20:59:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello")
+print("Hello, type your name :")
+n=str(input())
+print("Which place are you in?")
+p=str(input())
+print("Hello",n,"How is the weather in",p,"?")
+print("what is your age?")
+age=int(input())
+print("good know you are ",age,"years old")
+
+#this is ouyput
+#Hello, type your name :
+#om kumar
+#Which place are you in?
+#Darbhanga
+#Hello om kumar How is the weather in Darbhanga ?
+#what is your age?
+#20
+#good know you are  20 years old
