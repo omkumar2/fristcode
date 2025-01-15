9000bf045e611db6b0259c15df040e61f88cf167
omkumar2 <omkumariitbomboy@gmail.com>
2025-01-15 21:33:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,3 @@ print("what is your age?")
 age=int(input())
 print("good know you are ",age,"years old")
 
-#this is ouyput
-#Hello, type your name :
-#om kumar
-#Which place are you in?
-#Darbhanga
-#Hello om kumar How is the weather in Darbhanga ?
-#what is your age?
-#20
-#good know you are  20 years old
