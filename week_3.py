01a19cd4eceedc7d13f4d1550432e707bdbe88e0
omkumar2 <omkumariitbomboy@gmail.com>
2025-02-03 23:12:29 +0530

    Update week_3.py

--- week_3.py
+++ week_3.py
@@ -194,7 +194,101 @@ print("The answer is",ans)
 # nter a number:11
 # The answer is 55
 
+#for loop for multiplication tables
 
+for i in range(1,5):
+  print("2 X ", i, "=", 2*i)
+
+# 2 X  1 = 2
+# 2 X  2 = 4
+# 2 X  3 = 6
+# 2 X  4 = 8
+
+#More on range and for loop without range
+
+for x in range(1,11):
+  if(x % 2 != 0):
+   print(x)
+
+#next
+for x in range(9, -1, -1):
+  print(x)   
+
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+  
+#here 1 is the starting number, 11 is the ending number and 2 is the step size
+
+#next
+country = "India"
+for letter in country:
+  print(letter)
+
+# I
+# n
+# d
+# i
+# a
+
+# Formatted Printing
+
+for x in range( 10):
+  print(x, end = " ")  # 0 1 2 3 4 5 6 7 8 9
+
+#next
+d = 3
+m = 2
+y = 2025
+print("todays date is", end = ' ')
+print(d, m, y, sep = "-") # todays date is 3-2-2025
+
+#next
+num = int(input("enter a number: "))
+for i in range(1, 11):
+  #print(num, 'x', i, '=', num*i)
+  #print(f'{num} x {i} = {num*i}')
+  #print('%d X %d = %d' %(num, i, num*i))
+  print('{0} X {1} = {2}'.format(num, i, num*i))
+
+# enter a number: 5
+# 5 X 1 = 5
+# 5 X 2 = 10
+# 5 X 3 = 15
+# 5 X 4 = 20
+# 5 X 5 = 25
+# 5 X 6 = 30
+# 5 X 7 = 35
+# 5 X 8 = 40
+# 5 X 9 = 45
+# 5 X 10 = 50
+
+#next
+pi = 22 / 7
+print(f'Value of Pi = {pi}') #Value of Pi = 3.142857142857143
+print('Value of Pi = {0}'.format(pi)) #Value of Pi = 3.142857142857143
+print('Value of Pi = %f' % (pi)) #Value of Pi = 3.142857
+
+print(f'Value of Pi = {pi:.2f}') #Value of Pi = 3.14
+print('Value of Pi = {0:.2f}'.format(pi)) #Value of Pi = 3.14
+print('Value of Pi = %.4f' % (pi)) #Value of Pi = 3.1428
+
+#next
+print('{0:4d}'.format(1))
+print('{0:5d}'.format(11))
+print('{0:4d}'.format(111))
+print('{0:5d}'.format(1111))
+
+# 1
+#    11
+#  111
+#  1111
 
 
 
